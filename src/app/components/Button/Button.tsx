1aa0b5e7ac072be4ec40b48099f2d756f86b0608
ronald-it <ronald.cirgofer@hotmail.com>
2025-05-22 17:27:04 +0200

    Menu buttons now have conditional styling implemented.

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,16 +1,22 @@
+'use client';
 import { JSX } from 'react';
 import styles from './Button.module.scss';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 type ComponentProps = {
   children: JSX.Element;
   description: string;
-}
+  url: string;
+};
 
-export default function Button({children, description} : ComponentProps) {
+export default function Button({ children, description, url }: ComponentProps) {
+  const pathname = usePathname();
+
   return (
-    <button className={styles.button}>
+    <Link href={url} className={`${styles.button} ${pathname === url && styles['button--active']}`}>
       {children}
       <span className={styles.button__description}>{description}</span>
-    </button>
+    </Link>
   );
 }
--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+'use client';
 import styles from './Footer.module.scss';
 import Button from '../Button/Button';
 import HomeIcon from '../icons/HomeIcon';
@@ -5,24 +6,26 @@ import SearchIcon from '../icons/SearchIcon';
 import ArchivedIcon from '../icons/ArchivedIcon';
 import TagIcon from '../icons/TagIcon';
 import SettingsIcon from '../icons/SettingsIcon';
+import { usePathname } from 'next/navigation';
 
 export default function Footer() {
+  const pathname = usePathname();
   return (
     <footer className={styles.footer}>
-      <Button description="home">
-        <HomeIcon color='#335CFF' />
+      <Button description='home' url='/'>
+        <HomeIcon color={pathname === '/' ? '#335CFF' : '#525866'} />
       </Button>
-      <Button description="search">
-        <SearchIcon color='#525866' />
+      <Button description='search' url='/search'>
+        <SearchIcon color={pathname === '/search' ? '#335CFF' : '#525866'} />
       </Button>
-      <Button description="archived">
-        <ArchivedIcon color='#525866' />
+      <Button description='archived' url='/archive'>
+        <ArchivedIcon color={pathname === '/archive' ? '#335CFF' : '#525866'} />
       </Button>
-      <Button description="tags">
-        <TagIcon color='#525866' />
+      <Button description='tags' url='/tags'>
+        <TagIcon color={pathname === '/tags' ? '#335CFF' : '#525866'} />
       </Button>
-      <Button description="settings">
-        <SettingsIcon color='#525866' />
+      <Button description='settings' url='/settings'>
+        <SettingsIcon color={pathname === '/settings' ? '#335CFF' : '#525866'} />
       </Button>
     </footer>
   );
